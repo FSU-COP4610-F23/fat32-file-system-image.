900a8d359cc3ec47e8a31cc8e6fba75344864e59
akelpie <jdmasopeh@outlook.com>
2023-11-22 09:09:21 -0500

    made changes to part-4 of project

--- filesys.c
+++ filesys.c
@@ -71,13 +71,23 @@ int main(int argc, char *argv[])
     }
 
     // boot_sector_info bootInfo;
-    FileSystemState *fsState;
-    memset(&fsState, 0, sizeof(fsState));
+    // FileSystemState *fsState;
+    // memset(&fsState, 0, sizeof(fsState));
+
+    FileSystemState *fsState = malloc(sizeof(FileSystemState));
+    if (fsState == NULL)
+    {
+        perror("Failed to allocate memory for FileSystemState");
+        return 1;
+    }
+    memset(fsState, 0, sizeof(FileSystemState));
+
     // parse_boot_sector(file, &bootInfo);
     parse_boot_sector(file, fsState);
     // run_shell(image_path, &bootInfo);
     run_shell(image_path, fsState);
     close(file);
+    free(fsState);
     return 0;
 }
 
@@ -180,15 +190,15 @@ void parse_boot_sector(int file, FileSystemState *fsState)
     } // printf("BPB_FATSz32: %u\n", fsState->bootInfo.BPB_FATSz32);
 
     fsState->bootInfo.dataRegionAddress = fsState->bootInfo.BPB_BytsPerSec * fsState->bootInfo.BPB_RsvdSecCnt + // in decimal expression
-                              fsState->bootInfo.BPB_FATSz32 * fsState->bootInfo.BPB_NumFATs * fsState->bootInfo.BPB_BytsPerSec;
+                                          fsState->bootInfo.BPB_FATSz32 * fsState->bootInfo.BPB_NumFATs * fsState->bootInfo.BPB_BytsPerSec;
 
     // should be same as dataRegionAddress using formula
     fsState->bootInfo.rootClusPosition = fsState->bootInfo.dataRegionAddress + fsState->bootInfo.BPB_BytsPerSec * (fsState->bootInfo.BPB_RootClus - 2);
 
     // sectors in root directory
-     fsState->bootInfo.rootDirSectors = ((fsState->bootInfo.BPB_RootEntCnt * 32) + (fsState->bootInfo.BPB_BytsPerSec - 1)) / fsState->bootInfo.BPB_BytsPerSec;
+    fsState->bootInfo.rootDirSectors = ((fsState->bootInfo.BPB_RootEntCnt * 32) + (fsState->bootInfo.BPB_BytsPerSec - 1)) / fsState->bootInfo.BPB_BytsPerSec;
     // sectors in data region
-    fsState->bootInfo.dataSec = fsState->bootInfo.BPB_TotSec32 - (fsState->bootInfo.BPB_RsvdSecCnt + (fsState->bootInfo.BPB_NumFATs * fsState->bootInfo.BPB_FATSz32) +  fsState->bootInfo.rootDirSectors);
+    fsState->bootInfo.dataSec = fsState->bootInfo.BPB_TotSec32 - (fsState->bootInfo.BPB_RsvdSecCnt + (fsState->bootInfo.BPB_NumFATs * fsState->bootInfo.BPB_FATSz32) + fsState->bootInfo.rootDirSectors);
     // count of clusters in data region
     fsState->bootInfo.total_clusters = fsState->bootInfo.dataSec / fsState->bootInfo.BPB_SecPerClus;
     // size of image disk
@@ -226,38 +236,41 @@ void run_shell(const char *imageName, FileSystemState *fsState)
         }
         else if (strcmp(command, "open") == 0)
         {
-            char filename[256]; 
-            char mode[3]; 
+            char filename[256];
+            char mode[3];
             scanf("%s %s", filename, mode);
 
-            int already_opened = 0; 
-            for (int i=0; i<fsState->openedFilesCount; i++)
+            int already_opened = 0;
+            for (int i = 0; i < fsState->openedFilesCount; i++)
             {
                 if (strcmp(fsState->openedFiles[i].filename, filename) == 0)
                 {
-                    printf("Error: file already opened\n"); 
-                    already_opened = 1; 
-                    break; 
+                    printf("Error: file already opened\n");
+                    already_opened = 1;
+                    break;
                 }
             }
 
             if (!already_opened)
             {
-                int flags = 0; 
-                if (strcmp(mode, "-r") == 0) flags = O_RDONLY; 
-                else if (strcmp(mode, "-w") == 0) flags = O_WRONLY; 
-                else if (strcmp(mode, "-rw") == 0 || strcmp(mode, "-wr") == 0) flags = O_RDWR; 
-                else 
+                int flags = 0;
+                if (strcmp(mode, "-r") == 0)
+                    flags = O_RDONLY;
+                else if (strcmp(mode, "-w") == 0)
+                    flags = O_WRONLY;
+                else if (strcmp(mode, "-rw") == 0 || strcmp(mode, "-wr") == 0)
+                    flags = O_RDWR;
+                else
                 {
-                    printf("Error: Invalid mode\n"); 
-                    continue; 
+                    printf("Error: Invalid mode\n");
+                    continue;
                 }
 
-                int fd = open(filename, flags); 
-                if (fd == -1) 
+                int fd = open(filename, flags);
+                if (fd == -1)
                 {
-                    perror("Error opening file"); 
-                    continue; 
+                    perror("Error opening file");
+                    continue;
                 }
 
                 // Add to the openedFiles array
@@ -267,24 +280,117 @@ void run_shell(const char *imageName, FileSystemState *fsState)
                 fsState->openedFiles[fsState->openedFilesCount].offset = 0;
                 fsState->openedFilesCount++;
             }
-
-
         }
         else if (strcmp(command, "close") == 0)
         {
-            
+            char filename[256];
+            scanf("%s", filename);
+            int found = 0;
+            for (int i = 0; i < fsState->openedFilesCount; i++)
+            {
+                if (strcmp(fsState->openedFiles[i].filename, filename) == 0)
+                {
+                    close(fsState->openedFiles[i].file_descriptor);
+                    for (int j = i; j < fsState->openedFilesCount - 1; j++)
+                    {
+                        fsState->openedFiles[j] = fsState->openedFiles[j + 1];
+                    }
+                    fsState->openedFilesCount--;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                printf("Error: File not opened or does not exist\n");
+            }
         }
         else if (strcmp(command, "lsof") == 0)
         {
-
+            if (fsState->openedFilesCount == 0)
+            {
+                printf("No files are opened\n");
+            }
+            else
+            {
+                for (int i = 0; i < fsState->openedFilesCount; i++)
+                {
+                    printf("Index: %d, Filename: %s, Mode: %s, Offset: %ld\n",
+                           i, fsState->openedFiles[i].filename, fsState->openedFiles[i].mode, fsState->openedFiles[i].offset);
+                }
+            }
         }
         else if (strcmp(command, "lssek") == 0)
         {
-
+            char filename[256];
+            off_t offset;
+            scanf("%s %ld", filename, &offset);
+            int found = 0;
+            for (int i = 0; i < fsState->openedFilesCount; i++)
+            {
+                if (strcmp(fsState->openedFiles[i].filename, filename) == 0)
+                {
+                    if (offset >= 0 && offset <= lseek(fsState->openedFiles[i].file_descriptor, 0, SEEK_END))
+                    {
+                        fsState->openedFiles[i].offset = offset;
+                        found = 1;
+                    }
+                    else
+                    {
+                        printf("Error: Invalid offset\n");
+                    }
+                    break;
+                }
+            }
+            if (!found)
+            {
+                printf("Error: File not opened or does not exist\n");
+            }
         }
         else if (strcmp(command, "read") == 0)
         {
-
+            char filename[256];
+            size_t size;
+            scanf("%s %zu", filename, &size);
+            int found = 0;
+            for (int i = 0; i < fsState->openedFilesCount; i++)
+            {
+                if (strcmp(fsState->openedFiles[i].filename, filename) == 0)
+                {
+                    if (strcmp(fsState->openedFiles[i].mode, "-r") == 0 || strcmp(fsState->openedFiles[i].mode, "-rw") == 0 || strcmp(fsState->openedFiles[i].mode, "-wr") == 0)
+                    {
+                        char *buffer = malloc(size);
+                        if (buffer == NULL)
+                        {
+                            printf("Error: Memory allocation failed\n");
+                            break;
+                        }
+                        ssize_t read_bytes = pread(fsState->openedFiles[i].file_descriptor, buffer, size, fsState->openedFiles[i].offset);
+                        if (read_bytes >= 0)
+                        {
+                            fsState->openedFiles[i].offset += read_bytes;
+                            buffer[read_bytes] = '\0';
+                            printf("%s\n", buffer);
+                        }
+                        else
+                        {
+                            printf("Error reading file\n");
+                        }
+                        free(buffer);
+                        found = 1;
+                        break;
+                    }
+                    else
+                    {
+                        printf("Error: File not opened for reading\n");
+                        break;
+                    }
+                }
+            }
+            if (!found)
+            {
+                printf("Error: File not opened or does not exist\n");
+            }
         }
         else
         {
