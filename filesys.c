ae905ee1ba0590d6db496302de27e98f7a55d8e3
juancarlos alguera <juancarlosalguera14@gmail.com>
2023-12-06 02:54:10 -0500

    trying to correct read

--- filesys.c
+++ filesys.c
@@ -764,7 +764,7 @@ bool custom_read(const char *filename, size_t size, FileSystemState *fsState)
             }
 
             //printf("File Size: %u\n", fileSize);
-            if (size > fileSize);
+            if (size > fileSize)
                 size = fileSize;
 
             // Allocate buffer for reading
